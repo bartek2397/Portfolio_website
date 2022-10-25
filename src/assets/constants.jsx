e8fb98061921d7754a7505e9d5e97f93096768bd
bartek2397 <bartosz.wszolek97@gmail.com>
2022-10-25 14:35:11 +0200

    Footer

--- src/assets/constants.jsx
+++ src/assets/constants.jsx
@@ -1,3 +1,8 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+
 export const images = [
     {
         id: 1,
@@ -24,10 +29,35 @@ export const links = [
         id: '1',
         name: 'GitHub',
         link: 'https://github.com/bartek2397',
+        icon: 
+        <>
+            <FontAwesomeIcon icon={faGithub} />
+        </>
     },
     {
         id: '2',
         name: 'LinkedIn',
         link: 'https://www.linkedin.com/in/bartek-wszo%C5%82ek-b7620b1b5/',
+        icon: <>
+        <FontAwesomeIcon icon={faLinkedin} />
+    </>
     }
 ];
+export const contacts = [
+    {
+        id: '1',
+        content: 'bartosz.wszolek97@gmail.com',
+        icon: 
+            <>
+                <FontAwesomeIcon icon={faEnvelope} />
+            </>
+    },
+    {
+        id: '2',
+        content: '+48 790 428 358',
+        icon: 
+            <>
+                <FontAwesomeIcon icon={faPhone} />
+            </>
+    }
+]
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,31 @@
 import React from 'react'
-import { Link } from '@mui/icons-material'
-import { GitHub } from '@mui/icons-material'
-import { Typography } from '@mui/material'
-import { links } from '../assets/constants'
+import { links, contacts } from '../assets/constants'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
 
 const Footer = () => {
   return (
     <section className='w-full h-[30vh] bg-gray-800'>
-        <div className='w-[1170px] mt-0 mb-0 mr-auto ml-auto pt-5 pb-0 pr-[15px] pl-[15px] text-left flex flex-col justify-between'>
-          <footer>
-            <h2>Contact</h2>
-            {links.map((item) => (
-            <div key={item.id}>
-                  <Typography>
-                    <Link to={item.link} ></Link>
-                  </Typography>
+        <div className='w-[1170px] h-[100%] mt-0 mb-0 mr-auto ml-auto pr-[15px] pl-[15px] text-left flex flex-col justify-between'>
+          <footer className='flex justify-between h-[100%]'>
+            <div className='w-[30%] self-center'>
+              <h2>Contact</h2>
             </div>
+            <div className='flex flex-col w-[30%] justify-center'>
+            {links.map((item) => (
+              <a key={item.id} href={item.link} target='_blank' className='text-white no-underline m-5 text-lg'>
+                {item.icon} {item.name}
+              </a>
             ))}
+            </div>
+            <div className='w-[30%] flex flex-col justify-center'>
+              {contacts.map((contact) => (
+                <p className='text-white text-lg'>
+                  {contact.icon} {contact.content}
+                </p>
+              ))}
+            </div>
             
           </footer>
         </div>
