bd8732aa2238327670d22f104bcee93d3fa12073
bartek2397 <bartosz.wszolek97@gmail.com>
2022-11-24 21:57:43 +0100

    .

--- src/sections/AboutMe.jsx
+++ src/sections/AboutMe.jsx
@@ -13,7 +13,7 @@ const AboutMe = () => {
           <path className='fill-[#40434E]' d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"></path>
         </svg>
       </div>
-        <div className={`max-w-[1170px] m-auto flex flex-col justify-center items-stretch md:flex-col md:justify-center md:items-center ${scrollPosition > 300 ? 'visible animate-fade-up animate-once animate-duration-1000 animate-ease-in-out animate-normal animate-fill-backwards' : 'invisible'}`}>
+        <div className={`max-w-[1170px] m-auto flex flex-col justify-center items-stretch md:flex-col md:justify-center md:items-center ${scrollPosition > 100 ? 'visible animate-fade-up animate-once animate-duration-1000 animate-ease-in-out animate-normal animate-fill-backwards' : 'invisible'}`}>
           <div className='flex flex-wrap'>
             <div className={`w-[100%] md:w-full md:text-center`}>
               <h1 className='md:fluid-5xl text-[#40434E]'>
