4bae6499229116904548a68837a183ef1d3f9931
bartek2397 <bartosz.wszolek97@gmail.com>
2022-12-02 15:08:33 +0100

    final fixes

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -12,9 +12,9 @@ const Intro = () => {
           <h2 className='text-[#C9C5CB]'>Junior Frontend developer</h2>
         </div>
         <div className='w-[50%] h-[50%] relative md:w-full md:fluid-md'>
-          <img src={Laptop} alt="Laptop" className='w-[80%] absolute bottom-[25%] xl:bottom-0 left-[0%] lg:bottom-[25%] md:w-[60%] md:left-[20%] sm:bottom-[40%] animate-fade-up animate-once animate-duration-1000 animate-ease-in-out animate-normal animate-fill-backwards'/>
-          <img src={Phone} alt="Phone" className='w-[30%] absolute bottom-[25%] xl:bottom-0 left-[0%] lg:bottom-[25%] md:w-[25%] md:bottom-[40%] md:left-[20%] sm:bottom-[40%] animate-fade-up animate-once animate-duration-1000 animate-delay-500 animate-ease-in-out animate-normal animate-fill-backwards'/>
-          <img src={Mouse} alt="Mouse" className='w-[30%] absolute bottom-[25%] xl:bottom-0 left-[50%] lg:bottom-[25%] md:w-[25%] md:bottom-[40%] md:left-[55%] sm:bottom-[40%] animate-fade-up animate-once animate-duration-1000 animate-delay-1000 animate-ease-in-out animate-normal animate-fill-backwards  '/>
+          <img src={Laptop} alt="Laptop" className='w-[80%] absolute bottom-[25%] xl:bottom-0 left-[0%] lg:bottom-[25%] md:w-[60%] md:left-[20%] md:-translate-y-[20%] animate-fade-up animate-once animate-duration-1000 animate-ease-in-out animate-normal animate-fill-backwards'/>
+          <img src={Phone} alt="Phone" className='w-[30%] absolute bottom-[25%] xl:bottom-0 left-[0%] lg:bottom-[25%] md:w-[25%] md:bottom-[40%] md:left-[20%] md:-translate-y-[40%] animate-fade-up animate-once animate-duration-1000 animate-delay-500 animate-ease-in-out animate-normal animate-fill-backwards'/>
+          <img src={Mouse} alt="Mouse" className='w-[30%] absolute bottom-[25%] xl:bottom-0 left-[50%] lg:bottom-[25%] md:w-[25%] md:bottom-[40%] md:left-[55%] md:-translate-y-[40%] animate-fade-up animate-once animate-duration-1000 animate-delay-1000 animate-ease-in-out animate-normal animate-fill-backwards  '/>
 
         </div>
     </div>
