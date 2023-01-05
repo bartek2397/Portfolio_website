586057c9bc3f19e0345a74b4e73ef0c450fe08aa
bartek2397 <bartosz.wszolek97@gmail.com>
2023-01-05 14:13:38 +0100

    images fixing

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,15 +6,15 @@ import Phone from '../assets/images/iPhone_Laying_R.png'
 
 const Intro = () => {
   return (
-    <div className='max-w-[1170px] h-screen flex items-center justify-center m-auto md:flex-col'>
+    <div className='max-w-[1170px] h-screen flex items-center m-auto md:flex-col'>
         <div className='w-[50%] h-[50%] pr-[15px] pl-[15px] md:w-full md:text-center md:pt-20 animate-fade-up animate-once animate-duration-1000 animate-ease-in-out animate-normal animate-fill-backwards'>
           <h1 className='text-[#C9C5CB]'>Bartek Wszolek</h1>
           <h2 className='text-[#C9C5CB]'>Junior Frontend developer</h2>
         </div>
         <div className='w-[50%] h-[50%] relative md:w-full md:fluid-md'>
-          <img src={Laptop} alt="Laptop" className='w-[90%] absolute bottom-[25%] left-[0%] lg:bottom-[25%] md:w-[60%] md:left-[20%] animate-fade-up animate-once animate-duration-1000 animate-ease-in-out animate-normal animate-fill-backwards'/>
-          <img src={Phone} alt="Phone" className='w-[40%] absolute bottom-[25%] left-[0%] lg:bottom-[25%] md:w-[25%] md:bottom-[40%] md:left-[20%] animate-fade-up animate-once animate-duration-1000 animate-delay-500 animate-ease-in-out animate-normal animate-fill-backwards'/>
-          <img src={Mouse} alt="Mouse" className='w-[40%] absolute bottom-[25%] left-[50%] lg:bottom-[25%] md:w-[25%] md:bottom-[40%] md:left-[55%] animate-fade-up animate-once animate-duration-1000 animate-delay-1000 animate-ease-in-out animate-normal animate-fill-backwards  '/>
+          <img src={Laptop} alt="Laptop" className='w-[90%] absolute bottom-[30%] right-[0%] lg:bottom-[25%] md:w-[60%] md:left-[20%] animate-fade-up animate-once animate-duration-1000 animate-ease-in-out animate-normal animate-fill-backwards'/>
+          <img src={Phone} alt="Phone" className='w-[40%] absolute bottom-[30%] left-[10%] lg:bottom-[25%] md:w-[25%] md:bottom-[40%] md:left-[20%] animate-fade-up animate-once animate-duration-1000 animate-delay-500 animate-ease-in-out animate-normal animate-fill-backwards'/>
+          <img src={Mouse} alt="Mouse" className='w-[40%] absolute bottom-[30%] left-[60%] lg:bottom-[25%] md:w-[25%] md:bottom-[40%] md:left-[55%] animate-fade-up animate-once animate-duration-1000 animate-delay-1000 animate-ease-in-out animate-normal animate-fill-backwards  '/>
 
         </div>
     </div>
